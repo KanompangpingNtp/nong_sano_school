bb897a82206c58de31119093059c6d1974fa6d61
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2024-12-25 17:35:45 +0700

    update

--- resources/views/user/home/home.blade.php
+++ resources/views/user/home/home.blade.php
@@ -2,6 +2,7 @@
 @extends('user.home.layouts.app')
 
 @section('content')
+
 @include('user.home.components.page-1')
 @include('user.home.components.page-2')
 @include('user.home.components.page-3', ['pressRelease' => $pressRelease])
@@ -11,4 +12,5 @@
 @include('user.home.components.page-7', ['classroom' => $classroom])
 @include('user.home.components.page-8', ['building' => $building])
 @include('user.home.components.page-9')
+
 @endsection
